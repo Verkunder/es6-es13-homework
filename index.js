9f53b78c984a609064cced403f83e8c5a20ca6c7
Kapustin Stepan <s.kapustin@webpractik.ru>
2023-02-08 15:49:39 +0300

    feat(symbol): добавил symbol и приватное свойство классов

--- index.js
+++ index.js
@@ -153,3 +153,19 @@
 // const { hi } = await import('./console.js')
 
 // hi()
+
+// const symbol = Symbol('Hello')
+// const name = Symbol('Hello')
+
+// console.log(symbol === name)
+
+// class ClassWithPrivateStaticField {
+// 	static #PRIVATE_STATIC_FIELD
+
+// 	static publicStaticMethod() {
+// 		ClassWithPrivateStaticField.#PRIVATE_STATIC_FIELD = 42
+// 		return ClassWithPrivateStaticField.#PRIVATE_STATIC_FIELD
+// 	}
+// }
+
+// console.log(ClassWithPrivateStaticField.publicStaticMethod() === 42)
